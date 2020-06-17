9b49d5de042439a2c1b942b7a948c20d5332dc77
Danielle Byrne <DanielleByrne421@gmail.com>
2020-06-17 01:06:33 -0400

    pulling api information, haven't gotten it to log to the page

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,19 @@
 $(document).ready(function () {
   // var APIkey= "62bc91b04494ffcca055cb79a046bbf8d";
-  var arrayOfCities= []
+  var arrayOfCities = [];
 
   $("#newSearch").on("click", function (event) {
     event.preventDefault();
     var searchCity = $("#searchCity").val();
-    arrayOfCities.push(searchCity)
+    arrayOfCities.push(searchCity);
     // console.log(searchCity);
     localStorage.setItem("savedCity", JSON.stringify(arrayOfCities));
     var getCity = JSON.parse(localStorage.getItem("savedCity"));
     // console.log(getCity);
-    getForecast(searchCity);
-
+    getCurrentForecast(searchCity);
   });
 
-
-  function getForecast(userCity) {
+  function getCurrentForecast(userCity) {
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       userCity +
@@ -25,12 +23,39 @@ $(document).ready(function () {
       method: "GET",
     }).then(function (response) {
       console.log(response);
+      console.log(response.name);
+      console.log(response.main.temp);
+      console.log(response.main.humidity);
+      console.log(response.wind.speed);
+      //   console.log(response.weather.0.icon)
+      console.log();
+    //   $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      $(".cityName").append("<h1>" + response.name + " Weather Details</h1>")
       $("#pastSearchedCities").append(
-          `<div class= 'card'>  <div class='card-body'> `+ userCity +` </div> </div>`)
-          
+        `<div class= 'card'>  <div class='card-body'> ` + userCity +` </div> </div>`
+      );
     });
   }
 });
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
 
 // In class activity code
 
